e50e0b4b5b315c1c652ee9c2ee912fb29dfef2e6
Napat96 <phechrsteam9@gmail.com>
2025-03-10 14:40:50 +0700

    Create age.py

---
+++ cell04/ex01/age.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# ขอให้ผู้ใช้ป้อนอายุ และแปลงเป็นตัวเลข
+age = int(input("Please tell me your age: "))
+
+# แสดงผลอายุปัจจุบัน
+print(f"You are currently {age} years old.")
+
+# คำนวณและแสดงอายุในอนาคต
+print(f"In 10 years, you'll be {age + 10} years old.")
+print(f"In 20 years, you'll be {age + 20} years old.")
+print(f"In 30 years, you'll be {age + 30} years old.")
