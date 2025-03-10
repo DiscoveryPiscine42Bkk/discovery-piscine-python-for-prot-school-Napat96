8393492194bbdadfc00e95da342c689bd18ba086
Napat96 <phechrsteam9@gmail.com>
2025-03-11 00:18:07 +0700

    Create upcase_it.py

---
+++ cell06/ex01/upcase_it.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Define the upcase_it method
+def upcase_it(input_string):
+    return input_string.upper()
+
+# Test the method with the string "hello"
+result = upcase_it("hello")
+print(result)
