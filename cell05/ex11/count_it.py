d59cc02faf5b17bc22fe68f9f2f510e87a9fb0c0
Napat96 <phechrsteam9@gmail.com>
2025-03-11 00:11:25 +0700

    Update count_it.py

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
+import sys
+
+# Check if there are no parameters
+if len(sys.argv) == 1:
+    print("none")
+else:
+    # Display the number of parameters
+    print(f"parameters: {len(sys.argv) - 1}")
+    
+    # Iterate over the parameters and display each with its length
+    for param in sys.argv[1:]:
+        print(f"{param} {len(param)}")
 
