302c5deaf59df7f5020e466c85c0f8ec2e37635d
Napat96 <phechrsteam9@gmail.com>
2025-03-11 00:04:07 +0700

    Create parameters.py

---
+++ cell05/ex04/parameters.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+
+# Get the number of arguments (excluding the script name)
+num_parameters = len(sys.argv) - 1
+
+# Print the number of parameters
+print(num_parameters)
