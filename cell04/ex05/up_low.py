050f750e8a15d6f4542cabd0a853106112794fae
Napat96 <phechrsteam9@gmail.com>
2025-03-10 14:44:24 +0700

    Create up_low.py

---
+++ cell04/ex05/up_low.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# ขอให้ผู้ใช้ป้อนข้อความ
+text = input("Enter a string: ")
+
+# สลับตัวพิมพ์ใหญ่และพิมพ์เล็ก
+swapped_text = text.swapcase()
+
+# แสดงผลลัพธ์
+print(swapped_text)
